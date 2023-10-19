57d66eba47f2126dfed9379e3828c2c7c81a2c7a
p0lygun <solankivibhakar82@gmail.com>
2023-10-20 03:02:11 +0530

    feat(api): add Entry model

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Entry(models.Model):
+    time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    description = models.TextField(blank=False)
+    calories = models.PositiveIntegerField(help_text="Total Number of calories in an entry", null=False)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+    def __str__(self):
+        return self.description[0:20]
