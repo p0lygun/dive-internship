93734622793ab55b33edb2ce63edbc6c9e002e51
p0lygun <solankivibhakar82@gmail.com>
2023-10-22 03:56:01 +0530

    feat(calpy:accounts): create and set permission on groups if not exists

--- src/accounts/apps.py
+++ src/accounts/apps.py
@@ -1,6 +1,40 @@
+import os
+
 from django.apps import AppConfig
+from loguru import logger
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN'):
+            logger.info("Setting up accounts app config....")
+
+            from django.contrib.auth.models import Group
+            from django.contrib.auth.models import Permission
+
+            GROUPS = {
+                "normal": ['entry'],
+                "manager": ['customuser'],
+                "admin": ['entry', 'customuser']
+            }
+            PERMISSIONS = ['add', "change", "view", "delete"]
+            for group, models in GROUPS.items():
+                group: Group
+                group, created = Group.objects.get_or_create(name=group)
+                if created:
+                    logger.info(f'{group} Created')
+                    group_permissions = []
+                    for model in models:
+                        for permission_name in PERMISSIONS:
+                            codename = f"{permission_name}_{model}"
+                            permission = Permission.objects.get(codename=codename)
+                            group_permissions.append(permission)
+                    group.permissions.set(group_permissions)
+                else:
+                    logger.info(f'{group} Exists')
+
+
+
