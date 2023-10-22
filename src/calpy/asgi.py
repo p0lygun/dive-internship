84e26a700776e79c3439cf5a004bee8e835df65b
p0lygun <solankivibhakar82@gmail.com>
2023-10-23 03:50:51 +0530

    chore(calpy): added readme.md

--- src/calpy/asgi.py
+++ src/calpy/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calpy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calpy.settings.dev')
 
 application = get_asgi_application()
